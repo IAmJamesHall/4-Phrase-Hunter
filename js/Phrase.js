41c408e97ce72f05ffac304f3d66bb8600329ad0
James Hall <thatwhichendureth@gmail.com>
2019-12-02 21:14:58 -0500

    Work on Phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,28 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase;
+    }
+
+    addPhraseToDisplay() {
+        const ul = document.createElement('ul');
+
+        for (letter of this.phrase) {
+            if (letter === ' ') {
+                const li = document.createElement('li');
+                li.className = "space";
+                li.textContent = " ";
+            } else {
+                const li = document.createElement('li');
+                li.classList = `hide letter ${letter}`;
+                li.textContent = letter;
+            }
+            ul.appendChild(li);
+        }
+
+
+    }
+}
